273701839cda3d422bc3f45c9037cf51fd19b33c
Amir <sefati100@gmail.com>
2024-10-11 15:01:38 +0330

    refactor model.py and views.py in user model

--- user/views.py
+++ user/views.py
@@ -3,18 +3,28 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializer import UserSerialize
 from drf_yasg.utils import swagger_auto_schema
-
+from .models import User
 
 class UserCreateView(APIView):
 
     @swagger_auto_schema(
         request_body=UserSerialize,
         responses={201: UserSerialize(), 400: 'Bad Request'},
-        operation_description="Create a user profile with a user and bio"
+        operation_description="Create a user"
     )
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         serializer = UserSerialize(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            validated_data = serializer.validated_data
+            email = validated_data.get('email')
+            password = validated_data.get('password')
+
+            #I use stackoverflow to find this line
+            extra_fields = {k: v for k, v in validated_data.items() if k not in [
+                'email', 'password']}
+            user = User.objects.create_user(
+                email=email, password=password, **extra_fields)
+            response_serializer = UserSerialize(user)
+
+            return Response(response_serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
